da210132102c9ccecdb007c8ea1339e8be9cb6c4
= <=>
2024-07-07 16:37:13 +0530

    Raising custom events

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
 })
 export class FavoriteComponent{ 
+
+  @Input()
   isFavorite =  false;
+  @Output()
+  change = new EventEmitter();
 
   onClick(){
     this.isFavorite = !this.isFavorite;
+    this.change.emit();
   }
 }
